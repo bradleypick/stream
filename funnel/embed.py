3f88fdcaa2ac6901ec280cc74c0ee0fd0ee9ba60
Bradley Pick <bradleypick@gmail.com>
2020-06-30 12:54:06 -0700

    numpy and correlation of embeddings

--- funnel/embed.py
+++ funnel/embed.py
@@ -1,5 +1,7 @@
+import numpy as np
 import tensorflow_hub as hub
 
+# TODO: download the model in the dockerfile so embed.py doesn't have to do it
 embed = hub.load("https://tfhub.dev/google/universal-sentence-encoder/4")
 
 embeddings = embed([
@@ -10,4 +12,4 @@ embeddings = embed([
     "I am a sentence for which I would like to get its embedding"
 ])
 
-print(embeddings)
+print(np.corrcoef(np.array(embeddings)))
